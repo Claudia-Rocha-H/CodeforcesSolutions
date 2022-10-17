5f6cc5be505e5fccef9890f8695a91244612f5ab
Claudia Rocha <115832200+Claudia-Rocha-H@users.noreply.github.com>
2022-10-16 19:09:58 -0500

    50A - DominoPïling

---
+++ DominoPiling.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class DominoPiling {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(),m = sc.nextInt();
+		int res = (n*m)/2;
+		System.out.println(res);
+
+	}
+
+}
